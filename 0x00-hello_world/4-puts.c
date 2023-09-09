48ff1021d6dcff8074bc37699d60f45bf44f9914
ifarrag <ifarrrag@gmail.com>
2023-09-08 23:57:40 -0700

    THe puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - The starting point of the program
- * Return:0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzl");//puts: Print on screen
+	puts("\"Programming is like building a multilingual puzzl");/**puts: Print on screen*/
 	return (0);
 }
