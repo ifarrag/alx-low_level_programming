742ba77cbf04a275f7886b5ea20192ee11833bac
ifarrag <ifarrrag@gmail.com>
2023-09-13 17:12:59 -0700

    fprintf Function to print stderr and stdout

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 /**
  * main - Entry point of the program
  * fprintf - Display the stdout and stderr
- * return: 1 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
