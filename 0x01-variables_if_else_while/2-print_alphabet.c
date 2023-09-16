e106a3cadd963ac5681637c2ab131f3bf24cb511
ifarrag <ifarrrag@gmail.com>
2023-09-16 06:06:50 -0700

    The alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry poin
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 97 ; x <= 122; x++)
+	{
+		putchar(x);
+	}
+	return (0);
+}
