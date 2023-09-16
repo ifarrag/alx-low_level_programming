106354e6d124178f259b47b1be78e47b5cd02108
ifarrag <ifarrrag@gmail.com>
2023-09-16 13:08:26 -0700

    0123456789

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+return (0);
+}
