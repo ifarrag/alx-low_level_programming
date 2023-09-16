e53877c0c580a483ec4ceb5381be2bd345cb197c
ifarrag <ifarrrag@gmail.com>
2023-09-16 06:20:27 -0700

    The alphabetes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		putchar(x);
+	}
+	for (x = 65; x <= 90; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
