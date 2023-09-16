dfaab478678a55daead16807a51eadd6f8a7a622
ifarrag <ifarrrag@gmail.com>
2023-09-16 12:16:58 -0700

    0123456789

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigend short int x = 0;
+
+	do {
+		printf("&d", x);
+	} while (x <= 9);
+	printf("\n");
+return (0);
+}
