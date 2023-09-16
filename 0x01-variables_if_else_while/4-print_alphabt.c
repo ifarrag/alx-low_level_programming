94d8cb0587a004ad17eb75f82ee1d85fa69bbdcd
ifarrag <ifarrrag@gmail.com>
2023-09-16 11:53:04 -0700

    Print not number of alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 96; x <= 122; x++)
+	{
+		if ((x != 101) && (x != 113))
+		{
+			putchar(x);
+		}
+	}
+	putchar('\n');
+return (0);
+}
