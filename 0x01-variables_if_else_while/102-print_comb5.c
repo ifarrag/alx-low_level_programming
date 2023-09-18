bdd17fc7ce34c05560525a6457910c22f382d787
ifarrag <ifarrrag@gmail.com>
2023-09-18 06:17:13 -0700

    from 0 to 99

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,28 +5,27 @@
  **/
 int main(void)
 {
-	unsigned short int x1, x2, x3, x4, x12total, x34total;
+	unsigned short int x1, x2, x3, x4, x12total, x34total, xta;
 
 	for (x1 = 48; x1 <= 57; x1++)
 	{
 		for (x2 = 48; x2 <= 57; x2++)
 		{
-
 			for (x3 = 48; x3 <= 57; x3++)
 			{
-
-				for (x4 = 48; x4 <= 57; x4++)
+			for (x4 = 48; x4 <= 57; x4++)
 				{
 				x12total = x1 + x2;
 				x34total = x3 + x4;
-				if (!(x12total >= x34total))
+				xta = x12total + x34total;
+				if (x12total < x34total)
 				{
 					putchar(x1);
 					putchar(x2);
 					putchar(32);
 					putchar(x3);
 					putchar(x4);
-					if (!((x12total == 133) && (x34total == 114)))
+					if (xta != 227)
 					{
 						putchar(',');
 						putchar(32);
