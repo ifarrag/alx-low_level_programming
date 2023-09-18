782a6383b1ccfa3072309a073261b5083e3f8048
ifarrag <ifarrrag@gmail.com>
2023-09-18 04:47:48 -0700

    00 01 and 01 00

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	unsigned short int x1, x2, x3, x4, x12total, x34total;
+
+	for (x1 = 48; x1 <= 57; x1++)
+	{
+		for (x2 = 48; x2 <= 57; x2++)
+		{
+
+			for (x3 = 48; x3 <= 57; x3++)
+			{
+
+				for (x4 = 48; x4 <= 57; x4++)
+				{
+				x12total = x1 + x2;
+				x34total = x3 + x4;
+				if (!(x12total >= x34total))
+				{
+					putchar(x1);
+					putchar(x2);
+					putchar(32);
+					putchar(x3);
+					putchar(x4);
+					if (!(x12total == 133 && x34total == 114))
+					{
+						putchar(',');
+						putchar(32);
+					}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
