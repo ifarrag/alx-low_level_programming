7f303823688acb520b54f3b1d9c2d2b99be66241
ifarrag <ifarrrag@gmail.com>
2023-09-18 02:55:13 -0700

    Two digits combin

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	unsigned short int x, y, sum;
+
+	sum = x + y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+			if (!(x >= y))
+			{
+				putchar(x);
+				putchar(y);
+				if (sum != 113)
+				{
+					putchar(',');
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar('\n');
+return (0);
+}
