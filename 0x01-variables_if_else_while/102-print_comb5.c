fef47f4ad83f1d98f4440367ff4ba81fe87ec923
ifarrag <ifarrrag@gmail.com>
2023-09-19 02:10:48 -0700

    (00 01, ---> 98 99, )

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,33 +5,29 @@
  **/
 int main(void)
 {
-	unsigned short int x1, x2, x3, x4;
 
-	for (x1 = 48; x1 <= 57; x1++)
+unsigned short int x1, x2, x3, x4;
+
+for (x1 = 48; x1 <= 57; x1++)
+{
+	for (x2 = 48; x2 <= 57; x2++)
 	{
-		for (x2 = 48; x2 <= 57; x2++)
+	for (x3 = x1; x3 <= 57; x3++)
+	{
+	for (x4 = x2 + 1; x4 <= 57; x4++)
+	{
+		if ((x2 != x4) && (x3 != x1))
 		{
-			for (x3 = 48; x3 <= 57; x3++)
-			{
-			for (x4 = 48; x4 <= 57; x4++)
-				{
-				if (x3 >= x1)
-				{
-				if (!(x2 >= x4))
-				{
-					putchar(x1);
-					putchar(x2);
-					putchar(32);
-					putchar(x3);
-					putchar(x4);
-					putchar(',');
-					putchar(32);
-				}
-				}
-				}
-			}
+		putchar(x1);
+		putchar(x2);
+		putchar(32);
+		putchar(x3);
+		putchar(x4);
+		putchar(',');
+		putchar(32);
 		}
-	}
+	}}}
+}
 	putchar('\n');
 return (0);
 }
