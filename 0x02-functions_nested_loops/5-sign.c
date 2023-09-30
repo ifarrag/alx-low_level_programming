273f4b0a7f6491300e1062ea7177dd54ad6dac99
ifarrag <ifarrrag@gmail.com>
2023-09-30 04:37:08 -0700

    prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: The numper to check
+ * Return: 0 OR 1 OR -1 (success)
+ **/
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	else if (n > 0)
+	{
+		return (1);
+		_putcahr('+');
+	}
+	else
+	{
+		return ('-1');
+	}
+}
