694a2763606e54514f449b57be773c61e8f82802
ifarrag <ifarrrag@gmail.com>
2023-10-03 13:30:32 -0700

    Prints 10 times the numbers, from 0 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,7 @@ void more_numbers(void)
 {
 	unsigned short int n0t14, count;
 
-	for (count = 0; count <= 10; count++)
+	for (count = 0; count < 10; count++)
 	{
 		for (n0t14 = 0; n0t14 <= 14; n0t14++)
 		{
