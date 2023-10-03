1ab2e8f74e0dd1e759997f2bcf7c394ecee8f5e8
ifarrag <ifarrrag@gmail.com>
2023-10-02 19:40:53 -0700

    Prints the numbers, from 0 to 9 but not 4 & 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,12 @@ void print_most_numbers(void)
 {
 	unsigned short int num;
 
-	for (num = 0; num <=9; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		if (num != 2 && num != 4)
 		{
 			_putchar(num + '0');
 		}
 	}
+	_putchar(10);
 }
