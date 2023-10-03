85fd24c89b6a6b0f03fc2ce1635263b230619663
ifarrag <ifarrrag@gmail.com>
2023-10-02 19:31:38 -0700

    Prints the numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers 0-9
+ **/
+
+void print_numbers(void)
+{
+	unsigned short int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}
