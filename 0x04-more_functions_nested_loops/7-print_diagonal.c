4c1920a42341402e9b0e7253287d4dd42758bcdd
ifarrag <ifarrrag@gmail.com>
2023-10-03 15:21:54 -0700

    Draws a diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,22 +11,26 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		if (n > 1)
-		{
-			for (; n > 0; n--)
-			{
-				for (count = 1; count <= n - 1; count++)
+				if (n > 1)
 				{
-					_putchar(32);
+					count = n -1;
+
+					for (; n > 0; n--)
+					for (; count < n; count++)
+					{
+						if (n > 1)
+						{
+							_putchar(32);
+						}
+					}
+					_putchar('\\'), _putchar(10);
 				}
-				_putchar('\\');
-				_putchar(10);
+
+				else
+				{
+					_putchar('\\'), _putchar(10);
+				}
 			}
-		}
-		else
-		{
-			_putchar('\\'), _putchar(10);
-		}
 
 	}
 	else
