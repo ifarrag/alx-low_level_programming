aa9df2c9f212659e5a4b3793e29f576498e6d27f
ifarrag <ifarrrag@gmail.com>
2023-10-05 12:29:10 -0700

    Fizz | Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 != 0)
 		{
@@ -24,13 +24,14 @@ int main(void)
 
 		else if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("Fizz Buzz ");
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			printf("%d ", n);
 		}
 	}
-		printf("\n");
+
+	printf("\n");
 	return (0);
 }
