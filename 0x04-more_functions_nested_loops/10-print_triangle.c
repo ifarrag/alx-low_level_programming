668b553c6af48452e19100060b31a008003d9b7c
ifarrag <ifarrrag@gmail.com>
2023-10-09 02:52:23 -0700

    Use the character # to print the triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: size of the triangle
+ **/
+
+void print_triangle(int size)
+{
+	int n1, n2 = 1, n3 = 1;
+
+	if (size > 0)
+	{
+		while (n3 <= size)
+		{
+			n1 = size - n3;
+			while (n1 > 0)
+			{
+				n1--;
+				_putchar(32);
+			}
+			while (n2 <= n3 && n2 > 0)
+			{
+				n2++;
+				_putchar('#');
+			}
+			_putchar(10);
+			n3++;
+		}
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -23,6 +23,6 @@ void print_square(int size);
 
 void print_triangle(int size);
 
-
+void print_triangle(int size);
 
 #endif
