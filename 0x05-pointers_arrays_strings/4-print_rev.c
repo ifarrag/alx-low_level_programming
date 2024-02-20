c3495f3fbb86111bd3030d7fbc408e90813a976a
ifarrag <ifarrrag@gmail.com>
2024-02-19 17:58:34 -0800

    function that prints a string, in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: pointer to sring
+ */
+void print_rev(char *s)
+{
+	char *start = s;
+	
+	while (*s)
+	{
+
+		s++;
+	}
+	for (; s != start; s--)
+	{
+		_putchar(*s);
+	}
+	_putchar('\n');
+}
