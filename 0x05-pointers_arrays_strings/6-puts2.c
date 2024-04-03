bb722fdf84efb4da30cd2acd204e2e9c15622861
ifarrag <ifarrrag@gmail.com>
2024-04-03 07:17:25 -0700

    Filter string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,3 +1,17 @@
-#include <main.h>
+#include <sring.h>
+#include "main.h"
 /**
- *
+ * puts2 - prints every other character
+ * @str : pointer to string
+ */
+void puts2(char *str)
+{
+	int i, len = strlen(*str);
+
+	for (i = 0, i < len - 1, i++)
+	{
+		_putchar(str);
+		_putchar('\n');
+		str = str + 2;
+	}
+}
