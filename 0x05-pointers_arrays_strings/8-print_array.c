b96ac41363dd927ea55f36ef3cc2e78cd0f0903f
ifarrag <ifarrrag@gmail.com>
2024-04-04 18:54:29 -0700

    print sorted array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_array - in sorting
+ * @a : point to array
+ * @n : array elements
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		printf("%d",a[i]);
+		if (i != n-1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
