7d00b863b6e94ee94257ddad0311ca12f5574085
ifarrag <ifarrrag@gmail.com>
2024-04-06 17:02:03 -0700

    concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,18 @@
 char *_strcat(char *dest, char *src)
 {
 	int i,* len, n = 0;
+
 	len = &n;
 
 	for (i = 0; *dest != '\0'; i++)
 	{
 		dest++;
-		len++;
+		(*len)++;
 	}
 	for (i = 0; *src != '\0'; i++)
 	{
 		dest[n] = src[i];
-		len++;  
+		(*len)++;
 	}
 
 	dest[n] = '\0';
