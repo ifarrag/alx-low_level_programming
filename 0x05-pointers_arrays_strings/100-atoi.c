e2160eccf3cff3526d080f6e4ce438f7c1e6c6bf
ifarrag <ifarrrag@gmail.com>
2024-04-10 00:41:32 -0700

    convert a string to an integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,19 +6,24 @@
  */
 int _atoi(char *s)
 {
-	int num = 0;
+	int num = 0, i = 0;
 
 	while (*s)
 	{
 		if (*s >= 48 && *s <= 57)
 		{
-			if (*s-- == 45)
+			if (*s-- == 45 && *(s - 2) == 43)
 			{
-				num = -((*s - 48) + num * 10);
+				num = -((*s - 48) + num * (i * 10));
 			}
-			num = *s - 48 + num * 10;
 
+			else
+			{
+				num = (*s - 48) + num *(i * 10);
+			}
+			i = 1;
 		}
+
 		s++;
 	}
 
