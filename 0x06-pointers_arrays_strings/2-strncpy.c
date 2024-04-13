e67771b9d37d4a8dcf5e746fff69e425db36325b
ifarrag <ifarrrag@gmail.com>
2024-04-13 10:16:38 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+/**
+ * *_strncpy - copies a string
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: n bytes to copy from src
+ * Return: (dest)_
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int s =0;
+
+	while (s < n && s <= strlen(dest))
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		s++;
+	}
+	dest = dest - s;
+
+	return (dest);
+}
