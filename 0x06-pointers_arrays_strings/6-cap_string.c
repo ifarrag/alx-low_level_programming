dc2037b2608b9f206222282a6da4e4a30c62d87f
ifarrag <ifarrrag@gmail.com>
2024-04-14 09:10:26 -0700

    capitalizes all words after Separators

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,14 +8,14 @@ char *cap_string(char *str)
 {
 	int i = 0;
 	char *s = str;
-	char sep[] = "\n\t ,;.!?\"(){}";
+	char *sep = "\n\t ,;.!?\"(){}";
 
 	while (*str)
 	{
 		if (*str >= 'a' && *str <= 'z')
 		{
 			i = 0;
-			while (sep[i] == '\0')
+			while (sep[i] != '\0')
 			{
 				if (*(str - 1) == sep[i])
 					*str = *str - 32;
