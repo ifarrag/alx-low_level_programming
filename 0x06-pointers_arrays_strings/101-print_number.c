38295a5f6dc9bb7c87d08bea67430d5d56277377
ifarrag <ifarrrag@gmail.com>
2024-04-16 09:25:31 -0700

    Print number with putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+ * print_number - prints an integer
+ * @n: Integer to putchar
+ */
+void print_number(int n)
+{
+	int num = n, i = 0, ins;
+	char str;
+
+	while (n)
+	{
+		n = n / 10;
+		i++;
+	}
+
+	if (num < 0)
+		_putchat('-');
+
+	while (i > 0)
+	{
+		i--;
+		ins = (num / (10 ^ i)) % 10;
+		str = ins + 48;
+		_putchar(str);
+	}
+
+}
