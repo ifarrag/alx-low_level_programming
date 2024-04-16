6463beb1c1e357f48cd6f4e69b7f9cb3fefb2cb4
ifarrag <ifarrrag@gmail.com>
2024-04-16 07:51:33 -0700

    encodes a string using rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *rot13 - encodes a string using rot13
+ * @str: pointer to chars
+ * Return: (s)
+ */
+char *rot13(char *str)
+{
+	short int sign = 1;
+	char *s = str;
+
+	while (*str)
+	{
+		while ((*str >= 48 && *str <= 57) || (*str >= 65 && *str <= 122));
+		{
+			if (*str >= 'n' || *str >= 'N')
+				sign = -1;
+			*str = *str + (13 * sing);
+		}
+		sign = 1;
+
+		str++;
+	}
+
+	return (s);
+}
