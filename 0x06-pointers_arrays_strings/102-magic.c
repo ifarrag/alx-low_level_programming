90dcb324fc75ee50df5ee76dc9d77a7390e8bcd6
ifarrag <ifarrrag@gmail.com>
2024-04-17 14:42:58 -0700

    MMMMMMMMMM

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,8 +16,14 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-  *(&n - 3) = 98;
+  *(p + 3) = 98;
+  *(p + 2) = 99;
+  *(p + 1) = 100;
+  *(p + 0) = 101;
+  *(p - 1) = 102;
+  *(p - 2) = 103;
+  *(p - 3) = 104;
   /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
+  printf("a[2] = %d %d %d %d %d\n", a[2], a[0], a[1], a[3], a[4]);
   return (0);
 }
