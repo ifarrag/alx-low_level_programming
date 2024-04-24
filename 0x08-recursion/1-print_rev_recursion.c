3f94950304ef30a140d5ef1993046abb0d78edfe
ifarrag <ifarrrag@gmail.com>
2024-04-24 08:15:27 -0700

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,9 +8,12 @@ void _print_rev_recursion(char *s)
 {
 	char a;
 	int len0 = strlen(&s[0]), len = strlen(s);
+	int s_x = len0 - len;
 
-	if (len <= len0 / 2)
+	if ((len0 % 2 == 0 && len == len0 / 2) || (len0 % 2 != 0 && len == (len0 + 1) / 2))
+	{
 		return;
+	}
 
 	a = *s;
 	*s = *(&s[0] + (len - 1));
