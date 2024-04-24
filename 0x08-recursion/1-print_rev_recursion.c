79d0e2474927d9c24e213d79d1c46d7df116351f
ifarrag <ifarrrag@gmail.com>
2024-04-24 15:30:50 -0700

    Reverse Str

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,12 @@
  */
 void _print_rev_recursion(char *s)
 {
-	char a;
-	int len0 = strlen(&s[0]), len = strlen(s);
+	char a, *str = &s[0];
+	int len0 = strlen(str), len = strlen(s);
 
 	a = *s;
-	*s = *(&s[0] + (len - 1));
-	*(&s[0] + (len - 1))  = a;
+	*s = *(str[len - 1]);
+	*(str[len - 1])  = a;
 	s++;
 
 	if (len > (len0 / 2) && len0 % 2 == 0)
