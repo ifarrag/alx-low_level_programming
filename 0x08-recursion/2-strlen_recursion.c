1c8ea11970cb538974bb0d410bfb1c8282a56daa
ifarrag <ifarrrag@gmail.com>
2024-04-24 16:11:37 -0700

    Length of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get the length of string
+ * @s: pointer to char
+ */
+int _strlen_recursion(char *s)
+{
+	if (s == '\0')
+		return (s - &s[0]);
+	s++;
+	_strlen_recursion(s);
+}
