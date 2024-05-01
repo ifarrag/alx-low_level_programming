4107dd7f588b4d806666880b6bf870231ad65d65
ifarrag <ifarrrag@gmail.com>
2024-05-01 14:46:24 -0700

    Factorial n!

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - factorial of a given number
+ * @n: int num
+ * Return: factorial of a given number
+ */
+int factorial(int n)
+{
+	int sum = 1;
+
+	if (n == 0 || n == 1)
+		return (sum);
+	if (n < 0)
+		return (-sum);
+
+	sum = n * factorial(n-1);
+	return (sum);
+}
