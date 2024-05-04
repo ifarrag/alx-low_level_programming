9cb8027dbebd3df5fc2ed4cdb69210d243c41d29
ifarrag <ifarrrag@gmail.com>
2024-05-03 17:04:44 -0700

    square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: integer
+ * Return: (res) and (-1)
+ */
+int _sqrt_recursion(int n)
+{
+	int res = 1;
+
+	if (n > 1)
+	{
+		res = n / 2;
+		if ((res * res) == n)
+			return (res);
+		_sqrt_recursion(n - 1);
+	}
+	else if (n == 1)
+		return (res);
+	else if (n == 0)
+		return (0 * res);
+	else
+		return (-res);
+}
