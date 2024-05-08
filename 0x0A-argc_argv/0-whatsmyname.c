98338b0bacfdae77e083859801c71af95b29bcf5
Ifarrag <ifarrrag@gmail.com>
2024-05-08 05:06:26 -0700

    print program name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * main - Print argv[0]
+ * @argc: unused variable
+ * @argv: command line arguments
  * return: (0)
  */
 int main(int argc __attribute__((unused)), char **argv)
 {
-	for (; **argv != 32; argv++)
+	for (; **argv != 32; *argv++)
 	{
 		_putchar(**argv);
 	}
