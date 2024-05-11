de97e6cd852044a2b17395160f46ccb33adb121f
Ifarrag <ifarrrag@gmail.com>
2024-05-11 14:23:06 -0700

    Duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,8 +11,8 @@ char *_strdup(char *str)
 	unsigned int i;
 	char *str_d;
 
-	if (*str == 0)
-		return ('\0');
+	if (*str == '')
+		return ('');
 	str_d = malloc(sizeof(char) * strlen(str) + sizeof(char));
 	if (!str_d)
 		return ('\0');
