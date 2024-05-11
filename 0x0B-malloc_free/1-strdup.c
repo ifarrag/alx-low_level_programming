3afee410cd73063c0c54ee035b8db42158b78445
Ifarrag <ifarrrag@gmail.com>
2024-05-11 07:37:53 -0700

    newly allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,11 +13,11 @@ char *_strdup(char *str)
 	if (str)
 	{
 		str_d = malloc(strlen(str) + 1);
-		if (*str_d = '\0')
+		if (*str_d == '\0')
 			return ('\0');
 		for (i = 0; i <= strlen(str); i++)
 		{
-			*str_d[i] = *str[i];
+			str_d[i] = str[i];
 		}
 		return (str_d);
 	}
