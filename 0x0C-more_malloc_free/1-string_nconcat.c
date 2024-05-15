a901fcab3874e36ae94da07833325c896283bc9b
Ifarrag <ifarrrag@gmail.com>
2024-05-15 14:13:34 -0700

    malloc S1 + first n char(s) 0f S2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; i < t1; i++)
 	{
 		ptr[i] = s1[i];
 	}
