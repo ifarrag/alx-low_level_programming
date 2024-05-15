ff14288810a3d65fd0a18abbfd872c75173034ef
Ifarrag <ifarrrag@gmail.com>
2024-05-15 12:57:37 -0700

    Allocate memo

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * malloc_checked - 
+ * malloc_checked - allocate bytes of memo
  * @b: number of bytes to allocate
- * REturn: void pointer
+ * Return: void pointer
  */
 void *malloc_checked(unsigned int b)
 {
