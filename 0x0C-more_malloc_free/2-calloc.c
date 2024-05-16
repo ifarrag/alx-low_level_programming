9118984013a9ed4b5aa6f0c597c31a6c41df3dc9
Ifarrag <ifarrrag@gmail.com>
2024-05-16 04:06:21 -0700

    calloc ArrAy

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocate array
+ * @nmemb: NUmber bytes
+ * @size: sizeof(type)
+ * Return: void pointer to new alloc
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (!nmemb || !size)
+		return ('\0');
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return ('\0');
+
+	for (i = 0; i < nmemb * size; i++;)
+	       ptr[i] = '\0';
+	return (ptr)	;
+}
