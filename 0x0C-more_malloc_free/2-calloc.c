b6bdcd605cf6a2171aea455b0f764a975a6ba13a
Ifarrag <ifarrrag@gmail.com>
2024-05-16 04:11:08 -0700

    calloc ArrAy

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == 0)
 		return ('\0');
 
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	       ptr[i] = '\0';
 	return ((void *)ptr);
 }
