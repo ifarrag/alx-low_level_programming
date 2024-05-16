58c7b6ad5baa1ae81cb820a29077ae71ffdd80ae
Ifarrag <ifarrrag@gmail.com>
2024-05-16 06:28:27 -0700

    reallocation

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - reallocate memo with new size
+ * @ptr: pointer to allocate memo
+ * @old_size: old size of ptr
+ * @new_size: New size allocation
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *str;
+	unsigned int i;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return ('\0');
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	str = malloc(new_size);
+	if (str == 0)
+	{
+		free(ptr);
+		return ('\0');
+	}
+
+	for (i = 0; (char *)ptr != '\0' && i < old_size; i++)
+	{
+		str[i] = ptr[i];
+	}
+	free(ptr);
+	return ((void *)str);
+}
