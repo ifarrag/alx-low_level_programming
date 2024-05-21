29313b3e05eb231aedc3bca415db8a0fba1de0bd
Ifarrag <ifarrrag@gmail.com>
2024-05-21 16:01:36 -0700

    Call by reference

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,17 +1,5 @@
+#include "dog.h"
 #include <stdio.h>
-/**
- * struct dog - Poppy details
- * @name: His name
- * @age: His age
- * @owner: His Father
- */
-struct dog
-{
-        char *name;
-        float age;
-        char *owner;
-};
-
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: pointer to struct type
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,4 +3,17 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
+/**
+ * struct dog - Poppy details
+ * @name: His name
+ * @age: His age
+ * @owner: His Father
+ */
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+};
+
 #endif
