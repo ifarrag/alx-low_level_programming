2f275b9099e642fa14cf1aa6d119acaeea6b64e3
Ifarrag <ifarrrag@gmail.com>
2024-05-21 16:02:24 -0700

    Call by reference

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
  * struct dog - Poppy details
  * @name: His name
@@ -15,5 +13,5 @@ struct dog
         float age;
         char *owner;
 };
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
