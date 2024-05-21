df1543f9563d7025eb1ea69a6ebaf19c951c3701
Ifarrag <ifarrrag@gmail.com>
2024-05-21 16:36:06 -0700

    Print struct dog members

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,27 +14,22 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 		{
-			printf("Name: (nil)");
+			printf("Name: (nil)\n");
 		}
 		else
 		{
-			printf("Name: %s", d->name);
+			printf("Name: %s\n", d->name);
 		}
-		if (d->age == 0)
-		{
-			printf("Age: nil");
-		}
-		else
-		{
-			printf("Age: %.6f", d->age);
-		}
+
+		printf("Age: %.6f\n", d->age);
+		
 		if (d->owner == NULL)
 		{
-			printf("Owner: (nil)");
+			printf("Owner: (nil)\n");
 		}
 		else
 		{
-			printf("Owner: %s", d->owner);
+			printf("Owner: %s\n", d->owner);
 		}
 	}
 }
