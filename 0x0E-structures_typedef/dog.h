999124a4a1ee6475fb43e2f249c1231ec3ac16a2
Ifarrag <ifarrrag@gmail.com>
2024-05-20 17:05:19 -0700

    Function To initialize struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
  * struct dog - Poppy details
  * @name: His name
  * @age: His age
  * @owner: His Father
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
