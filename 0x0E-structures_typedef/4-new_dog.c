fac8205ba206c34f5d587fa4d072511912934da5
Ifarrag <ifarrrag@gmail.com>
2024-05-21 18:00:36 -0700

    return a pointer to struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: New name
@@ -10,17 +11,30 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *n = name;
-	char *o = owner;
+	int i = 0;
+	dog_t new;
+	dog_t *d = &new;
+	char *n = malloc(sizeof(char) * (strlen(name) + 1));
+	char *o = malloc(sizeof(char) * (strlen(owner) + 1));
 
+	for (i = 0; name != NULL; i++)
+	{
+		n[i] = name[i];
+	}
+	n[i] = '\0';
+	for (i = 0; owner != NULL; i++)
+	{
+		o[i] = owner[i];
+	}
+	o[i] = '\0';
 	if (name == NULL || owner == NULL)
 	{
+		free(new);
+		free(d);
+		free(n);
+		free(o);
 		return (NULL);
 	}
-	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
 	d->name = n;
 	d->age = age;
 	d->owner = o;
