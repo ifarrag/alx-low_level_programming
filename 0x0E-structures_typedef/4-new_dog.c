17d515cec3d2121312deb9e81ef38ec3d3328821
Ifarrag <ifarrrag@gmail.com>
2024-05-21 17:17:07 -0700

    return a pointer to struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,14 +8,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t new;
+	dog_t *d = &new;
+
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
-	dog_t new;
 	new.name = name;
 	new.age = age;
 	new.owner = owner;
-	dog_t *d = &new;
 	return (d);
 }
