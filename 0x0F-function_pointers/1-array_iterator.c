8c47dddd90ab5f2220a369d4c28bdb82b93324a7
Ifarrag <ifarrrag@gmail.com>
2024-05-22 16:37:24 -0700

    executes a function given as a parameter

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * array_iterator - Execute sized array by fun point par
+ * @array: Array to execute
+ * @size: size of the array
+ * @action: put an address of Function here
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+
+	while (i < size && action != NULL)
+	{
+		(*action)(array[i])
+		i++;
+	}
+}
