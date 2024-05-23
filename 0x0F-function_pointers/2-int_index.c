5541729dbf317c2deac4d7a5942f14335ccf5cc1
Ifarrag <ifarrrag@gmail.com>
2024-05-23 07:13:33 -0700

    searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+/**
+ * int_index - Search for an int
+ * @array: Search from here
+ * @size: Size of array
+ * @cmp: execute with index if found
+ * Return: index or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, r;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		r = cmp(array[i]);
+		if (r = 1)
+			return (i);
+	}
+	return (-1);
+}
