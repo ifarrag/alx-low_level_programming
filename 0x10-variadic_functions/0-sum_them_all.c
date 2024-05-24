d65c9af7da5dab53125646debe1da48711cbdace
Ifarrag <ifarrrag@gmail.com>
2024-05-24 13:15:08 -0700

    Sum an unfixed parameters

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,7 +7,8 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i = 0;
+	int sum = 0;
+	unsigned int i = 0;
 	va_list ptr;
 
 	if (n == 0)
@@ -18,5 +19,5 @@ int sum_them_all(const unsigned int n, ...)
 		sum += va_arg(ptr, int);
 	}
 	va_end(ptr);
-	return (sum)
+	return (sum);
 }
