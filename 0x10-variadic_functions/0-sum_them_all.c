bb417cf264357c2f042b4f06868ab78114d7337e
Ifarrag <ifarrrag@gmail.com>
2024-05-24 13:10:04 -0700

    Sum an unfixed parameters

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - Sum all my parameters
+ * @n: number of parameters
+ * @...: unfixed arguments
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0, i = 0;
+	va_list ptr;
+
+	if (n == 0)
+		return (0);
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return (sum)
+}
