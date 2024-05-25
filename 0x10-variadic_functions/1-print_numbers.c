69e71be28f485b559437a6f149e2accc800000ca
Ifarrag <ifarrrag@gmail.com>
2024-05-25 00:43:53 -0700

    Print N integers with a separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptr;
 
 	if (n == 0)
-		return (0);
+		return;
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(ptr, int));
-		if (separator != NULL && i != (n-1))
-			printf("%s", *separator);
+		if (separator != NULL || i != (n-1))
+			printf("%s", separator);
 	}
 	va_end(ptr);
 	printf("\n");
