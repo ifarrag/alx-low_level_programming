0d5da4be65251103bc53334c0d025cfdbc014fb8
Ifarrag <ifarrrag@gmail.com>
2024-05-26 16:11:29 -0700

    Print alll

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything
+ * @format: string of types
+ */
+void print_char()
+{
+	printf("%c"va_arg(ptr, char));
+}
+void print_string()
+{
+	char *str = va_arg(ptr, char*);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+void print_int()
+{
+	printf("%d", va_arg(ptr, int));
+}
+void print_float()
+{
+	printf("%f", va_arg(ptr, float));
+}
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	char arr[] = {c, s, i, f};
+	va_list ptr;
+	void (*fun[])() = {print_char, print_string, print_int, print_float};
+
+	va_start(ptr, format);
+	while (format != NULL)
+	{
+		while (i < 4)
+		{
+			if (*format == arr[i])
+			{
+				(*fun[i])();
+			}
+			i++;
+		}
+		format++;
+	}
+}
