fba2d658396f6cbac03f8a9b9ec2b179ac15a37d
Ifarrag <ifarrrag@gmail.com>
2024-05-26 16:54:30 -0700

    101try

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,11 +7,13 @@
 void print_char(va_list ptr)
 {
 	char cc = (char)va_arg(ptr, int);
+
 	printf("%c", cc);
 }
 void print_string(va_list ptr)
 {
 	char *str = va_arg(ptr, char*);
+
 	if (str == NULL)
 	{
 		printf("(nil)");
@@ -22,31 +24,36 @@ void print_string(va_list ptr)
 void print_int(va_list ptr)
 {
 	int integer = va_arg(ptr, int);
+
 	printf("%d", integer);
 }
 void print_float(va_list ptr)
 {
 	float ff = (float)va_arg(ptr, double);
+
 	printf("%f", ff);
 }
 void print_all(const char * const format, ...)
 {
-	unsigned int i = 0, n = 0;
+	unsigned int i = 0;
 	char arr[] = {'c', 's', 'i', 'f'};
 	va_list ptr;
-	void (*fun[])() = {print_char, print_string, print_int, print_float};
+	char *ptf = format;
+	void (*fun[])(va_list) = {, print_string, print_int, print_float};
 
 	va_start(ptr, format);
-	while (format[n] != '\0')
+	while (ptf != '\0')
 	{
 		while (i < 4)
 		{
-			if (*format == arr[i])
+			if (*ptf == arr[i])
 			{
-				(*fun[i])();
+				(*fun[i])(ptr);
 			}
 			i++;
 		}
-		n++;
+		ptf++;
 	}
+	printf('\n');
+	va_end(ptr);
 }
