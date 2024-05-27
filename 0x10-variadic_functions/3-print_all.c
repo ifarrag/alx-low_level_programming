797371e9da2fb8ed27cdc5e5f017ac8bb0bfa90d
Ifarrag <ifarrrag@gmail.com>
2024-05-26 17:27:49 -0700

    try 9

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,6 +4,18 @@
 /**
  * print_all - prints anything
  * @format: string of types
+ *
+ * print_char - print char
+ * @ptr: va_list
+ *
+ * print_string - print string
+ * @ptr: va_list
+ *
+ * print_int - print int
+ * @ptr: va_list
+ *
+ * print_float - print float
+ * @ptr: va_list
  */
 void print_char(va_list ptr)
 {
@@ -11,6 +23,7 @@ void print_char(va_list ptr)
 
 	printf("%c", cc);
 }
+
 void print_string(va_list ptr)
 {
 	char *str = va_arg(ptr, char*);
@@ -44,11 +57,13 @@ void print_all(const char * const format, ...)
 	va_start(ptr, format);
 	while (format[n] != '\0')
 	{
+		i = 0;
 		while (i < 4)
 		{
 			if (format[n] == arr[i])
 			{
 				(*fun[i])(ptr);
+				printf(", ");
 			}
 			i++;
 		}
