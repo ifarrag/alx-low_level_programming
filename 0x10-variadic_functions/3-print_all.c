b5bb5f348090c9ac4a5980ede3b427015fe04483
Ifarrag <ifarrrag@gmail.com>
2024-05-26 18:53:29 -0700

    try 9

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,20 +1,7 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include <string.h>
 /**
- * print_all - prints anything
- * @format: string of types
- *
  * print_char - print char
- * @ptr: va_list
- *
- * print_string - print string
- * @ptr: va_list
- *
- * print_int - print int
- * @ptr: va_list
- *
- * print_float - print float
  * @ptr: va_list
  */
 void print_char(va_list ptr)
@@ -23,7 +10,10 @@ void print_char(va_list ptr)
 
 	printf("%c", cc);
 }
-
+/**
+ * print_string - print string
+ * @ptr: va_list
+ */
 void print_string(va_list ptr)
 {
 	char *str = va_arg(ptr, char*);
@@ -35,18 +25,30 @@ void print_string(va_list ptr)
 	}
 	printf("%s", str);
 }
+/**
+ * print_int - print int
+ * @ptr: va_list
+ */
 void print_int(va_list ptr)
 {
 	int integer = va_arg(ptr, int);
 
 	printf("%d", integer);
 }
+/**
+ * print_float - print float
+ * @ptr: va_list
+ */
 void print_float(va_list ptr)
 {
 	float ff = (float)va_arg(ptr, double);
 
 	printf("%f", ff);
 }
+/**
+ * print_all - prints anything
+ * @format: string of types
+ */
 void print_all(const char * const format, ...)
 {
 	unsigned int i = 0, n = 0;
@@ -55,7 +57,7 @@ void print_all(const char * const format, ...)
 	void (*fun[])(va_list) = {print_char, print_string, print_int, print_float};
 
 	va_start(ptr, format);
-	while (format[n] != '\0')
+	while (format[n] != '\0' && format != NULL)
 	{
 		i = 0;
 		while (i < 4)
