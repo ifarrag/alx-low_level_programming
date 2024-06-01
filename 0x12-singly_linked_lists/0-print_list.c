706769103dd65cd8adf2f2e9d19fbd7623e760a7
ifarrag <ifarrrag@gmail.com>
2024-06-01 11:11:12 -0700

    Print Data of nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lists.h>
+/**
+ * print_list - print data of nodes
+ * @h: pointer to list_t
+ * Return: size of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *ptr = h;
+	unsigned int size = 0;
+
+	while(ptr->next != NULL)
+	{
+		size++;
+		if(ptr->str != '\0')
+		{
+			printf("[%lu] %s", ptr->len, ptr->str);
+		}
+		else
+		{
+			printf("[0] (nill)");
+		}
+		ptr = ptr->next;
+	}
+	return (size);
+}
