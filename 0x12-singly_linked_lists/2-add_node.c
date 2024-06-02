654895ce36c76781841302c57fb3ee2824a8db06
ifarrag <ifarrrag@gmail.com>
2024-06-01 19:51:13 -0700

    Add New Node at the list head

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,10 +11,21 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *ptr = malloc(sizeof(list_t));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
 	if (head == NULL)
 		return (NULL);
 	ptr->str = strdup(str);
+	if (ptr->str == NULL)
+	{
+		free(ptr->str);
+		free(ptr);
+		return (NULL);
+	}
 	ptr->len = strlen(ptr->str);
 	ptr->next = *head;
 	*head = ptr;
