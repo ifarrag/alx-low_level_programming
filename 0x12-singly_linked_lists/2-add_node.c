4af4cc253114830b97786ced640ba00bd8ba8cf4
ifarrag <ifarrrag@gmail.com>
2024-06-01 19:31:20 -0700

    Add New Node at the list head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * add_node - Add new node at the head
+ * @head: Pointer to the head of the list
+ * @str: A string to duplicate in str
+ * Return: Pointer to THe NEw list_t (head)
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *ptr = malloc(sizeof(list_t));
+
+	if (head == NULL)
+		return (NULL);
+	ptr->str = strdup(str);
+	ptr->len = strlen(ptr->str);
+	ptr->next = *head;
+	*head = ptr;
+	return (*head);
+}
