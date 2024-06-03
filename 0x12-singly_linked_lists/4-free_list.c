3f7d320d60760452bc42e71d3e7898fb12b1f3f1
ifarrag <ifarrrag@gmail.com>
2024-06-03 16:23:58 -0700

    FREE LISTS

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - Free all list nodes
+ * @head: The start of the list
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr = head;
+
+	if (ptr == NULL)
+	{
+		return;
+	}
+	if (ptr->next == NULL)
+	{
+		free(ptr);
+		return;
+	}
+	ptr = ptr->next;
+	free_list(ptr);
+}
