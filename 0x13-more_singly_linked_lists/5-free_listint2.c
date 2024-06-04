3ee43eaabb9b6abba3513a490e24786fcc834228
ifarrag <ifarrrag@gmail.com>
2024-06-04 10:57:47 -0700

    Free The list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - Free all list
+ * @head: The start point
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t **temp = head;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = &(*temp)->next;
+		free(*head);
+		head = temp;
+	}
+}
