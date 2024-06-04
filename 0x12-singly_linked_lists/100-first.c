9c53cff5a5d15d3acdb21dfa8ff74711f2bbc220
ifarrag <ifarrrag@gmail.com>
2024-06-03 19:08:34 -0700

    Execute Function before main()

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * before_main - Print before main
+ */
+void before_main() __attribute__((constructor))
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
