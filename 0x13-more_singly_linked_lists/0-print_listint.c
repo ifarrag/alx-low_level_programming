dffcea57fa0946f1bd69e4ab883dcf65a5aaf78f
ifarrag <ifarrrag@gmail.com>
2024-06-03 23:03:13 -0700

    Print the Data

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * print_listint - Print list data
+ * @h: pointer to the start
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int sum = 0;
+
+	while (h != NULL)
+	{
+		sum++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (sum);
+}
