209747ffea4d1c3aba87a87968d2d22507992d90
ifarrag <ifarrrag@gmail.com>
2024-06-04 17:39:30 -0700

    Free ALLL

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,10 +6,12 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
+
+	temp = *head;
 	while (*head != NULL)
 	{
 		temp = temp->next;
