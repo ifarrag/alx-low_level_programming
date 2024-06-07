0f743eebfc703809d56bd157e65ed86d145fbc9c
ifarrag <ifarrrag@gmail.com>
2024-06-07 16:12:25 -0700

    Set a bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,7 +11,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > 512)
 		return (-1);
 	num = *n >> index & 0;
-	num = num << index | *n;
+	num = num << index & *n;
 	*n = num;
 	return (1);
 }
