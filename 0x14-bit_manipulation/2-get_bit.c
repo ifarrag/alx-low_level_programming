0ca65b1c2e13b3fa0d40010809f5c7362a7022ae
ifarrag <ifarrrag@gmail.com>
2024-06-07 01:19:31 -0700

    git the value of specific bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_bit - get the value in a bit
+ * @n: Integer example
+ * @index: Number of the bit
+ * Return: The value of the bit or (-1)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int res = 0;
+
+	res = n >> index & 1;
+	return (res);
+}
