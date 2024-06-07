d5a1180b822195f428568cc4cbe900a5c1ae199c
ifarrag <ifarrrag@gmail.com>
2024-06-06 21:37:32 -0700

    Convert Integer to binary new way

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_binary - convert int to binary and print the result
+ * @n: Integer num
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int max = 1, b = 1, num = 0;
+
+	while (n > max)
+	{
+		if (max * 2 > n)
+			break;
+
+		max *= 2;
+		b++;
+	}
+	num = max;
+	for (; b >= 1; b--)
+	{
+		if (n >= num)
+		{
+			_putchar('1');
+		}
+		else
+			_putchar(0);
+		num = 1;
+		while (num < max)
+		{
+			if (num * 2 == max)
+				break;
+			num = num * 2;
+		}
+		if (n > num)
+			n = n - max;
+		max = num;
+	}
+}
