6607938eefa15e5086fcb21d181f3695bb3bdb1c
ifarrag <ifarrrag@gmail.com>
2024-06-11 02:55:17 -0700

    Print a file to stdout

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * read_textfile - read a text file and print to stdout
+ * @filename: The text file
+ * @letters: Number of lettres to read
+ * Return: Number of letters successfuly printed or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fptr;
+	int c = 0;
+	unsigned long int i = 0;
+
+	if (filename == NULL)
+		return (0);
+
+	fptr = fopen(filename, "r");
+	while ((c = fgetc(fptr)) != EOF && i <= letters)
+	{
+		i += c;
+		_putchar(c);
+	}
+	fclose(fptr);
+	return (i);
+}
