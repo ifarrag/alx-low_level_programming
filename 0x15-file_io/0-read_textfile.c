646e573fd9acb6f33aab0219bd2d9897e87bf12d
ifarrag <ifarrrag@gmail.com>
2024-06-11 03:13:49 -0700

    Print a file to stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	fptr = fopen(filename, "r");
-	if (fptr = NULL)
+	if (fptr == NULL)
 		return (0);
 	while ((c = fgetc(fptr)) != EOF && i < letters)
 	{
