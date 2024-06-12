c453986803dc81df2c2d85d8bc7700a2175d5cf4
ifarrag <ifarrrag@gmail.com>
2024-06-12 05:19:12 -0700

    copies the content

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+/**
+ *
+ */
+int main(int argc, char **argv)
+{
+	int fd1, fd2, rd = -1, wr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while (rd != 0)
+	{
+		fd2 = open(argv[2], O_WRONLY | O_APPEND);
+		if (fd2 == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+		rd = read(fd1, buf, 1024);
+		if (rd = -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		wr = write(fd2, buf, 1024);
+		if (wr == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			exit(98);
+		}
+	}
+	if (close(fd1) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) != 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
