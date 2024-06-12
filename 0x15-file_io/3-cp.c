431a8e4187ab8bcffe276ca4b5a96eb4441fe9e2
ifarrag <ifarrrag@gmail.com>
2024-06-12 05:40:40 -0700

    copies the content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,32 +22,26 @@ int main(int argc, char **argv)
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	fd2 = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
-	}
-	if (close(fd2) != 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
 	}
 	while (rd != 0)
 	{
-		fd2 = open(argv[2], O_WRONLY | O_APPEND);
 		if (fd2 == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 		rd = read(fd1, buf, 1024);
 		if (rd == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		wr = write(fd2, buf, 1024);
