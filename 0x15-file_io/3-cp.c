342d8bb1dca93e97db06e28832e7be6397f4a61a
ifarrag <ifarrrag@gmail.com>
2024-06-12 05:42:12 -0700

    copies the content

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		wr = write(fd2, buf, 1024);
+		wr = write(fd2, buf, rd);
 		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
