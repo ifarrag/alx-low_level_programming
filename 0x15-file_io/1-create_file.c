0ce9caf52549894002f43133021bb5c01c790794
ifarrag <ifarrrag@gmail.com>
2024-06-12 03:59:23 -0700

    Create New File

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,6 @@
-#include "main.h"
-#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 /**
  * create_file - Create a new file
  * @filename: The file title
@@ -8,16 +9,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *fptr;
+	int file;
+	unsigned long int str = strlen(text_content) + 1;
 
 	if (filename == NULL)
 		return (-1);
-	fptr = fopen(filename, "w+");
-	if (fptr == NULL)
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		fprintf(fptr, "%s", text_content);
+		write(file, text_content, str);
 	}
 	return (1);
 }
