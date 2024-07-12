45c68546ac3f30fc8d5c5be93e540c52147e9960
ifarrag <ifarrrag@gmail.com>
2024-07-12 16:06:15 -0700

    prints all the elements

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,7 +5,7 @@
  * @h: the head of list
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	size_t num = 0;
 	if (h == NULL)
