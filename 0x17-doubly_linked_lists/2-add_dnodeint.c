876864d80672b3b649f9559245e19d927e92a2f9
ifarrag <ifarrrag@gmail.com>
2024-07-12 17:33:24 -0700

    add node at the begin

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	if (*head == NULL)
+	if (*head != NULL)
 		(*head)->prev = new;
 	new->prev = NULL;
 	new->next = *head;
