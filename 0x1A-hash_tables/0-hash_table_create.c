d55ddba6111ec8610c1a666fb61220c96d2de968
ifarrag <ifarrrag@gmail.com>
2024-07-28 17:53:49 -0700

    First Hash

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+/**
+ *
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ptr = NULL;
+	hash_node_t **array;
+	int i;
+
+	if (size == 0) return NULL;
+	ptr = malloc(sizeof(hash_table_t));
+	if (ptr == NULL) return NULL;
+
+	for (i = 0; i < size; i++)
+	{
+		array[i] = malloc(sizeof(ptr));
+	}
+	array[size] = NULL;
+
+	ptr->size = size;
+	ptr->array = array;
+
+	return ptr;
+}
