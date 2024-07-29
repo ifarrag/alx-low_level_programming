464dcc80a6389dde52f21a5e3d973a11a9b6b8ae
ifarrag <ifarrrag@gmail.com>
2024-07-28 17:59:16 -0700

    First Hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,9 @@
 #include "hash_tables.h"
 #include <stdlib.h>
 /**
- *
+ * hash_table_create - create new hash table
+ * @size: the size of the table
+ * Return: pointer to the new table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
