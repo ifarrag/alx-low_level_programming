a840095b1f8b2e850bb978973803742fb4e4e537
ifarrag <ifarrrag@gmail.com>
2024-07-29 03:54:25 -0700

    Make table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return NULL;
 
 	ptr->size = size;
-	ptr->array = &(malloc(sizeof(ptr) * size));
+	ptr->array = malloc(sizeof(ptr) * size);
 
 	return (ptr);
 }
