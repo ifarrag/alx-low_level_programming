f710552498b2c2a9ad795cb619380ba0023d0ac5
ifarrag <ifarrrag@gmail.com>
2024-07-29 04:14:18 -0700

    Key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index - generate the key
+ * @key: secret str
+ * @size: size of the table
+ * Return: key index of the **array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int temp = 0;
+
+	tmp = hash_djb2(key) % size;
+	return (tmp);
+}
